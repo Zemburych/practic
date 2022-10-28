8e750d4421c9c62eaf0e2443c1d64dcc4b369f5f
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-28 21:33:06 +0300

    задача решена

--- prac01/Program.cs
+++ prac01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число.\n");
+while (true)
+{
+    string input = Console.ReadLine();
+
+    Console.WriteLine("Вторая цифра - {1}", input, input[1]);
+    break;
+}
