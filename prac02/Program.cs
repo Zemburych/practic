00a80083a386b3392f6efa3f04582ff174622f9c
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-28 22:07:43 +0300

    набросали задачу 13

--- prac02/Program.cs
+++ prac02/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число.\n");
+int input = Convert.ToInt32(Console.ReadLine());
+string Text = Convert.ToString(input);
+if (Text.Length < 2)
+{
+    Console.WriteLine("третья цифра - " + Text[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
