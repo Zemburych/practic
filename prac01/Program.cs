0f3cf3d65c6befe7509b44e7e9af7466e7b7abc6
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-06 13:16:56 +0300

    двумерный массив

--- prac01/Program.cs
+++ prac01/Program.cs
@@ -1,10 +1,19 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.Clear();
 Console.WriteLine("Введите трехзначное число.\n");
-while (true)
-{
-    string input = Console.ReadLine();
 
-    Console.WriteLine("Вторая цифра - {1}", input, input[1]);
-    break;
+void num(int numbers, int count)
+{
+    while (count -- > 0)
+    {
+        if (numbers == "2")
+        {
+            console.writeLine("привет " + numbers + "!");
+        }
+    else 
+        {
+        console.writeLine("кто ты" + numbers);
+        }
+    }
+    
 }
